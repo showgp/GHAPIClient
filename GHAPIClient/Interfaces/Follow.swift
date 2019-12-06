4130c2f61314e8ceaa912b65a4d43f3b1cbede32
ray <showgpray@gmail.com>
2019-12-06 14:45:03 +0800

    通过 Carthage 引入 Alamofire 和 PromiseKit

---
+++ GHAPIClient/Interfaces/Follow.swift
@@ -0,0 +1,9 @@
+//
+//  Followers.swift
+//  GHAPIClient
+//
+//  Created by Rui Peng on 12/6/19.
+//  Copyright © 2019 rayy.top. All rights reserved.
+//
+
+import Foundation
---
+++ carthage.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/usr/local/bin/carthage update --platform iOS --cache-builds --no-use-binaries
